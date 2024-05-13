57245ab912dbe42b514c810a994747eb7b78559a
AlineTiuss <169068304+alinetiuss@users.noreply.github.com>
2024-05-13 21:24:00 +0000

    13 de maio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,13 @@ public class Main {
     System.out.println("               | Série: Carga Pesada | ");
     System.out.println("                ---------------------  ");
     System.out.println("");
-    System.out.println("   A série foi exibida originalmente de 22 de maio de 1979 até 2 de janeiro de 1981, e teve um revival de maior sucesso, estrelado pelos mesmos atores, exibida de 29 de abril de 2003 até 7 de setembro de 2007. ");
+    System.out.println(
+        "   A série foi exibida originalmente de 22 de maio de 1979 até 2 de janeiro de 1981, e teve um revival de maior sucesso, estrelado pelos mesmos atores, exibida de 29 de abril de 2003 até 7 de setembro de 2007. ");
     System.out.println("");
-    System.out.println("   Carga Pesada conta a história de Pedro (Antonio Fagundes) e Bino (Stênio Garcia), são dois amigos que se juntam para comprar um dos melhores caminhões do mercado. Para pagar a dívida e conseguir algum dinheiro, eles começam a fazer transportes de cargas pesadas pelas longas estradas do Brasil, enfrentando lugares perigosos, tempestades e assaltos. ");
-     System.out.println("");
-     System.out.println("Para saber mais desssa série, acesse o seguinte link com seu trailer. ");
-     System.out.println(" https://youtu.be/171iXZeoeF8?si=TUFCyHjKD2qN9sl9 ");
+    System.out.println(
+        "   Carga Pesada conta a história de Pedro (Antonio Fagundes) e Bino (Stênio Garcia), são dois amigos que se juntam para comprar um dos melhores caminhões do mercado. Para pagar a dívida e conseguir algum dinheiro, eles começam a fazer transportes de cargas pesadas pelas longas estradas do Brasil, enfrentando lugares perigosos, tempestades e assaltos. ");
+    System.out.println("");
+    System.out.println("Para saber mais desssa série, acesse o seguinte link com seu trailer. ");
+    System.out.println(" https://youtu.be/171iXZeoeF8?si=TUFCyHjKD2qN9sl9 ");
   }
 }
